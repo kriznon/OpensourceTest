75e223d135c917bb842f21e704b4bf4ae99acb5d
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-09 11:55:38 +0530

    removed compilation errors

--- test_catch/sample_example_UT.c
+++ test_catch/sample_example_UT.c
@@ -1,10 +1,15 @@
 //#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "../find_three_number_average.h"
+#include "../find_three_number_avg.h"
 
 
-TEST_CASE( "Average 3 numbers", "[AverageThreeBytes]" ) {
+TEST_CASE( "Average 3 numbers", "[find_three_number_average]" ) {
     REQUIRE( find_three_number_average(15,15,15) == 15 );
-    REQUIRE( find_three_number_average.h(15,16,17) == 16 );
+    REQUIRE( find_three_number_average(15,16,17) == 16 );
 }
-
+/*
+TEST_CASE( "Average 3 numbers to fail", "[find_three_number_average]" ) {
+    REQUIRE( find_three_number_average(15,15,15) == 17 );
+    REQUIRE( find_three_number_average(15,16,17) == 18 );
+}
+*/
