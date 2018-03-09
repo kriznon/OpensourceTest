f8eadaa46f826525165298d6c9226025da910ddd
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-09 11:09:49 +0530

    initial commit

---
+++ find_three_number_avg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int find_three_number_average(int a, int b, int c) {
+
+
+	return (a+b+c)/3;
+
+}
+
+
+int main() {
+
+	printf("%d\n",find_three_number_average(15,15,15));
+	return 0;
+
+}
