9de0422f46a5b0de05ba465f4857c385597fbd85
Nileshkumar Raghuvanshi <nileshkumar.raghuvanshi@gmail.com>
2018-03-09 11:44:46 +0530

    test case added

--- test_catch/sample_example_UT.c
+++ test_catch/sample_example_UT.c
@@ -1,10 +1,10 @@
 //#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "sample_example.h"
+#include "../find_three_number_average.h"
 
 
 TEST_CASE( "Average 3 numbers", "[AverageThreeBytes]" ) {
-    REQUIRE( AverageThreeBytes(15,15,15) == 15 );
-    REQUIRE( AverageThreeBytes(15,16,17) == 16 );
+    REQUIRE( find_three_number_average(15,15,15) == 15 );
+    REQUIRE( find_three_number_average.h(15,16,17) == 16 );
 }
 
